38e46a8ab62aae5a28e0491e9e9a711987d619c8
Benoit Marchant <marchant@mac.com>
2019-01-07 18:48:40 -0800

    Makes component creation declarative

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -4,10 +4,6 @@
 var Component = require("montage/ui/component").Component;
 var defaultNavigator = require("../../core/navigator").defaultNavigator;
 
-var ProductsView = require("../views/products-view.reel").ProductsView;
-var ProductDetailView = require("../views/product-detail-view.reel").ProductDetailView;
-var CartView = require("../views/cart-view.reel").CartView;
-var CheckoutView = require("../views/checkout-view.reel").CheckoutView;
 var ReceiptView = require("../views/receipt-view.reel").ReceiptView;
 
 /**
@@ -20,36 +16,39 @@ exports.Main = Component.specialize(/** @lends Main# */ {
         value: function () {
             this.navigator = defaultNavigator;
 
-            this.navigator.registerRoutes([
-                {
-                    component: new ProductsView(),
-                    id: 'products',
-                    children: [
-                        {
-                            component: new ProductDetailView(),
-                            id: 'detail'
-                        }
-                    ]
-                },
-                {
-                    component: new CartView(),
-                    id: 'cart'
-                },
-                {
-                    component: new CheckoutView(),
-                    id: 'checkout'
-                },
-                {
-                    component: new ReceiptView(),
-                    id: 'receipt'
-                }
-            ]);
         }
     },
 
     templateDidLoad: {
         value: function () {
+
+            this.navigator.registerRoutes([
+                {
+                    component: this.productsView,
+                    id: 'products',
+                    children: [
+                        {
+                            component: this.productDetailView,
+                            id: 'detail'
+                        }
+                    ]
+                },
+                {
+                    component: this.cartView,
+                    id: 'cart'
+                },
+                {
+                    component: this.checkoutView,
+                    id: 'checkout'
+                },
+                {
+                    component: this.receiptView,
+                    id: 'receipt'
+                }
+            ]);
+
             this.navigator.succession = this.succession;
+            
             this.navigator.route('products');
         }
     }
